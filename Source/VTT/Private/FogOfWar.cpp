30d2ae668c3b8b37662fc39f85cdfaf729015aa9
Mikołaj Stankiewicz <gilotyna815@gmail.com>
2024-11-16 20:32:29 +0100

    Fog of war system prototype

---
+++ Source/VTT/Private/FogOfWar.cpp
@@ -0,0 +1,30 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "FogOfWar.h"
+#include "Engine/TextureRenderTarget.h"
+
+// Sets default values
+AFogOfWar::AFogOfWar()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AFogOfWar::BeginPlay()
+{
+	Super::BeginPlay();
+	CreateTextureRender
+
+	
+}
+
+// Called every frame
+void AFogOfWar::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
---
+++ Source/VTT/Public/MyPostProcessVolume.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/PostProcessVolume.h"
+#include "MyPostProcessVolume.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VTT_API AMyPostProcessVolume : public APostProcessVolume
+{
+	GENERATED_BODY()
+
+	
+	
+};

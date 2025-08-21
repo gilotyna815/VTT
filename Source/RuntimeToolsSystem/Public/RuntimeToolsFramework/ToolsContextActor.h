a65516bfa57ede3177c2e74bbefd75ea906e52e6
gilotyna815 <gilotyna815@gmail.com>
2025-08-21 18:16:20 +0200

    Added the placeholder ToolsContextActor class
    TODO: Replace with VTTPawn

---
+++ Source/RuntimeToolsSystem/Public/RuntimeToolsFramework/ToolsContextActor.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/DefaultPawn.h"
+#include "ToolsContextActor.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RUNTIMETOOLSSYSTEM_API AToolsContextActor : public ADefaultPawn
+{
+	GENERATED_BODY()
+	
+};
